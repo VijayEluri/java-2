6f949d0266cb8788fd55f1eea2f3d485aaf4f68d
TDOAN1H\Thanh <doant@outlook.com>
2017-10-30 09:54:12 -0700

    Implement type to compute the common ancestor for two set of vertices

--- graph/wordnet/SAP.java
+++ graph/wordnet/SAP.java
@@ -21,37 +21,59 @@ public class SAP {
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        reset();
-        computeDistances(v, distFromV, null);
-        int ancestor = computeDistances(w, distFromW, distFromV);
-        if (ancestor > -1) {
-            return distFromV[ancestor] + distFromW[ancestor];
+        int ancestor_vertex = ancestor(v, w);
+        if (ancestor_vertex == -1)
+        {
+            return -1;
         }
-        return -1;
+        return distFromV[ancestor_vertex] + distFromW[ancestor_vertex];
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral
     // path; -1 if no such path
     public int ancestor(int v, int w) {
         reset();
-        computeDistances(v, distFromV, null);
-        int ancestor = computeDistances(w, distFromW, distFromV);
-        if (ancestor > -1) {
-            return ancestor;
+        Queue<Integer> q1 = new Queue<>();
+        q1.enqueue(v);
+        Queue<Integer> q2 = new Queue<>();
+        q2.enqueue(w);
+        computeDistances(q1, distFromV);
+        computeDistances(q2, distFromW);
+        return findAncestor(distFromV, distFromW);
+    }
+
+    int findAncestor(final int dist1[], final int dist2[])
+    {
+        int min_idx = -1;
+        int min_dist = Integer.MAX_VALUE;
+        for (int i = 0; i < dist1.length; i++)
+        {
+            if (dist1[i] > -1 && dist2[i] > -1 && min_dist > dist1[i] + dist2[i])
+            {
+                min_dist = dist1[i] + dist2[i];
+                min_idx = i;
+            }
         }
-        return -1;
+        return min_idx;
     }
 
     // length of shortest ancestral path between any vertex in v and any
     // vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        int ancestor_vertex = ancestor(v, w);
+        if (ancestor_vertex == -1)
+        {
+            return -1;
+        }
+        return distFromV[ancestor_vertex] + distFromW[ancestor_vertex];
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no
     // such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        computeDistances(v, distFromV);
+        computeDistances(w, distFromW);
+        return findAncestor(distFromV, distFromW);
     }
 
     // Reset
@@ -62,29 +84,16 @@ public class SAP {
         }
     }
 
-    // Compute distance from x to other vertices using BFS
+    // Compute shortest distances from set x to other vertices using BFS
     // and store values in distFromX
-    // 1. if distFromY is null then the function run BFS for all reachable vertices
-    //    then return -1
-    // 2. if distFromY is given then 2 possibilities
-    //    if reachable vertex v from x is not reachable from y
-    //      i.e. distFromY[v] = -1 for all v reachable from x
-    //        then return -1
-    //    if one reachable vertex v from x is also reachable from y
-    //      i.e. distFromY[v] > -1 for any v reachable from y
-    //        then return v
-    private int computeDistances(int x, int distFromX[], int distFromY[]) {
+    private void computeDistances(Iterable<Integer> s, int distFromX[]) {
         Queue<Integer> q = new Queue<>();
-        q.enqueue(x);
-        distFromX[x] = 0;
+        for (int x : s) {
+            q.enqueue(x);
+            distFromX[x] = 0;
+        }
         while (!q.isEmpty()) {
             int v = q.dequeue();
-            if (distFromY != null) {
-                assert distFromY.length == g.V();
-                if (distFromY[v] > -1) {
-                    return v;
-                }
-            }
             for (int w : g.adj(v)) {
                 // vertex w hasn't been visited
                 if (distFromX[w] == -1) {
@@ -93,7 +102,6 @@ public class SAP {
                 }
             }
         }
-        return -1;
     }
 
     // do unit testing of this class
